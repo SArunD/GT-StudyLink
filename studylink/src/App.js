0ad8a8da0db6b1d5444d8870e42d9b79a2e238bc
lengmelissa <91227863+lengmelissa@users.noreply.github.com>
2025-04-14 13:51:49 -0400

    cleaned up a little and added nonfunctional popup to add events

--- studylink/src/App.js
+++ studylink/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import './DemoApp.js';
-import DemoApp from './DemoApp.js';
+import CalendarApp from './CalendarApp.js';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
@@ -11,22 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <DemoApp/>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <TimePicker label="Basic time picker" />
-        </LocalizationProvider>
+        <CalendarApp/>
         <div id='calendar'></div>
       </header>
     </div>
--- studylink/src/CalendarApp.js
+++ studylink/src/CalendarApp.js
@@ -5,32 +5,34 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import { INITIAL_EVENTS, createEventId } from './event-utils'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+
+let calendarApi;
 
 
 export default function DemoApp() {
     const [currentEvents, setCurrentEvents] = useState([])
+    const [seen, setSeen] = useState(false)
+
+    function togglePop () {
+        setSeen(!seen);
+    };
   
     function handleDateSelect(selectInfo) {
-      let title = prompt('Please enter a new title for your event')
-      let calendarApi = selectInfo.view.calendar
-  
-      calendarApi.unselect() // clear date selection
-  
-      if (title) {
-        calendarApi.addEvent({
-          id: createEventId(),
-          title,
-          start: selectInfo.startStr,
-          end: selectInfo.endStr,
-          allDay: selectInfo.allDay
-        })
-      }
+        togglePop();
+        //************ CHANGE TO: NEW EVENT FORM *************************************
+    //   let title = prompt('Please enter a new title for your event')
+        calendarApi = selectInfo.view.calendar
     }
+
   
     function handleEventClick(clickInfo) {
-      if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-        clickInfo.event.remove()
-      }
+        //********** CHANGE TO: SHOW EVENT INFO ***********************
+    //   if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+    //     clickInfo.event.remove()
+    //   }
     }
   
     function handleEvents(events) {
@@ -64,6 +66,74 @@ export default function DemoApp() {
             */
           />
         </div>
+        <div>
+            <button onClick={togglePop}>Create a new study session</button>
+            {seen ? <CreateSession toggle={togglePop} /> : null}
+        </div>
       </div>
     )
   }
+
+function renderEventContent(eventInfo) {
+    return (
+        <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+        </>
+    )
+}
+
+function CreateSession(props) {
+    const [sName, setSName] = useState('')
+    const [sTag, setSTag] = useState('')
+
+    function handleLogin(e) {
+        e.preventDefault()
+        // Code to handle login goes here
+        props.toggle()
+    }
+
+    return (
+        <div className="popup">
+            <div className="popup-inner">
+                <h2>Create a Study Session</h2>
+                <form onSubmit={handleLogin}>
+                    <label>
+                        Name of Study Session:
+                        <input type="text" value={sName} onChange={e => setSName(e.target.value)} />
+                    </label>
+                    <label>
+                        Tags:
+                        <input type="text" value={sTag} onChange={e => setSTag(e.target.value)} />
+                    </label>
+                    <div>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <TimePicker label="Start time" />
+                        </LocalizationProvider>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <TimePicker label="End time" />
+                        </LocalizationProvider>
+                    </div>
+                    
+                    <button type="submit" onClick={addSession(sName, 'tempval', 'tempval')}>Add Session</button>
+                </form>
+                <button onClick={props.toggle}>Close</button>
+            </div>
+        </div>
+    )
+}
+
+function addSession(title, start, end) {
+    if (title) {
+        if (title) {
+            calendarApi.addEvent({
+              id: createEventId(),
+              title,
+              start,
+              end,
+              allDay: false
+            })
+          }
+    }
+    calendarApi.unselect() // clear date selection
+}
