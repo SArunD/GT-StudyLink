0dfea7b3f6c702a5dd544d26f47c0f4614f0863b
lengmelissa <91227863+lengmelissa@users.noreply.github.com>
2025-04-27 18:12:56 -0400

    small bug

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react'
 import Nav from 'react-bootstrap/Nav';
 
-import Nav from 'react-bootstrap/Nav';
-
 import { AuthContext } from "../utils/AuthContext"
 
 function Header() {
